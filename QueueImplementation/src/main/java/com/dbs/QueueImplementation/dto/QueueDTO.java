1fee7f2d15b6268984b1d8afc8c5b9152c02c7e8
HOME <HOME@DESKTOP-VDF0L3H>
2019-08-10 12:26:52 +0530

    Adding MessageDTO and service DTO

---
+++ QueueImplementation/src/main/java/com/dbs/QueueImplementation/dto/QueueDTO.java
@@ -0,0 +1,29 @@
+package com.dbs.QueueImplementation.dto;
+
+public class QueueDTO {
+	
+	private int id;
+	private String queueName;
+	private int size;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQueueName() {
+		return queueName;
+	}
+	public void setQueueName(String queueName) {
+		this.queueName = queueName;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+
+}
